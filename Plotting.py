acc8f522646812e50d4f2115de643b68fc1d1641
ppkart <76852444+ppkart@users.noreply.github.com>
2024-02-14 12:54:45 +0200

    Update Plotting.py
    Added a windowed function for plotting.

--- Plotting.py
+++ Plotting.py
@@ -91,6 +91,61 @@ def plot_monthly_profiles(ax, fig, data_input, years, var_names, offset=9, offse
     return ax
 
 
+def plot_monthly_profiles_windowed(data_input, years, var_names, offset=9, offsety_scale=0, colors=[]):
+    """
+    Function for plotting the average daily profiles as a windowed figure.
+
+    Inputs:
+        data_input: Pandas dataframe object with datetimeindex in hourly resolution
+        years: List containing years from which data will be used. 
+               If many years are given, an average over all years are plotted
+        var_names: List of column names of data_input that will be included in the aggregation
+        offset: A float value for changing the x-position the month labels
+        offsety_scale: A float value for changing the y-position the month labels
+        colors: List containing colors for each variable
+
+    Output:
+        ax: Matplotlib Axes object where the data was plotted
+        fig: Matplotlib Figure object
+    """
+
+    # Get the monthly data and variable names
+    data, var_names = get_monthly_profiles(data_input, years, var_names)
+    data_windowed = []
+
+    for i in range(12):
+        if i > 0:
+            data_windowed.append(data[i*24-1:i*24+24,:])
+        else:    
+            data_windowed.append(data[i*24:i*24+24,:])
+
+    # Plot profiles
+    plt.rc('font', size=10)
+    figsize = (2244/300, 2000/300) # In pixels
+    
+    fig, ax = plt.subplots(3,4,figsize=figsize, sharex=True, sharey=True, gridspec_kw = {'wspace':0, 'hspace':.1})
+    #months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+    
+    xticks = list(range(0,12*24,4))
+
+    j = 0
+    for a in [ax[0,0], ax[0,1], ax[0,2], ax[0,3], ax[1,0], ax[1,1], ax[1,2], ax[1,3], ax[2,0], ax[2,1], ax[2,2], ax[2,3]]:
+        d = data_windowed[j]
+        a.set_xticks(xticks)
+        a.set_xticklabels((list(range(0,24))*12)[::4], fontsize=7, rotation=0)
+
+        j += 1
+        for i in range(0,d.shape[1]):
+            a.plot(d[:, i], label=var_names[i]
+                    , c=colors[i]
+                    )
+
+    ax[0,1].legend(loc='lower center', ncol=data.shape[1], bbox_to_anchor=(1,1))
+    plt.tight_layout()
+
+    return ax, fig
+
+
 if __name__ == "__main__":
     # Demonstration
 
@@ -110,5 +165,6 @@ if __name__ == "__main__":
     ax[2] = plot_monthly_profiles(ax=ax[2], fig=fig, data_input=data, years=[2019, 2020], var_names=['sin', 'cos'])
     plt.show()
 
-    
+    fig, ax = plot_monthly_profiles_windowed(data_input=data, years=[2019], var_names=['sin', 'cos'], colors=['red', 'blue'])
+    plt.show()
 
